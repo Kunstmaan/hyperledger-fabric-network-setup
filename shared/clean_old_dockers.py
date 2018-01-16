4cac8447d8d78006beb99a3fec6980dc8e4fca9b
Leurquin <guillaume.leurquin@kunstmaan.be>
2018-01-16 11:42:49 +0100

    add automatic image removal

--- shared/clean_old_dockers.py
+++ shared/clean_old_dockers.py
@@ -34,38 +34,51 @@ def call(script, *args):
 # The output of docker ps sorts the names with the most recent container first
 # This is important for the rest of the script, because we want to keep the most
 # recent version of each chaincode
-output = call("docker ps --format '{{.Names}}' | grep dev-").split('\n')
+outputContainers = call("docker ps --format '{{.Names}}' | grep dev-").split('\n')
+outputImages = call("docker images | grep 'dev-' | awk 'BEGIN { FS=\"[ ]\" } ; { print $1 }'").split('\n')
 
 # Test data, debugging only
 # output = ['dev-epeer1.vlaanderen.be-ground_contours-1.0.17', 'dev-epeer1.vlaanderen.be-ground_contours-1.0.16', 'dev-epeer1.vlaanderen.be-ground_contours-1.0.15', 'dev-epeer1.vlaanderen.be-subsidies-1.0.3', 'dev-epeer1.vlaanderen.be-nature_coins-1.0.3', 'dev-epeer1.vlaanderen.be-identity_management-1.0.3', 'dev-epeer1.vlaanderen.be-ground_contours-1.0.14', 'dev-epeer1.vlaanderen.be-access_management-1.0.3', 'dev-epeer1.vlaanderen.be-work_contracts-1.0.3', 'dev-epeer1.vlaanderen.be-nature_management_plan_contours-1.0.3', 'dev-epeer1.vlaanderen.be-certificates-1.0.3', 'dev-epeer1.vlaanderen.be-contours-1.0.3', 'dev-epeer1.vlaanderen.be-nature_coins-1.0.2', 'dev-epeer1.vlaanderen.be-subsidies-1.0.2', 'dev-epeer1.vlaanderen.be-certificates-1.0.2', 'dev-epeer1.vlaanderen.be-contours-1.0.2', 'dev-epeer1.vlaanderen.be-nature_management_plan_contours-1.0.2', 'dev-epeer1.vlaanderen.be-identity_management-1.0.2', 'dev-epeer1.grb.vlaanderen.be-identity_management-1.0.2', 'dev-epeer1.grb.vlaanderen.be-identity_management-1.0.1', '', 'test-test1-cc1-1.0.1', 'test-test1-cc1-1.0.0']
 
-output = filter(None, output) # Remove empty strings
+outputContainers = filter(None, outputContainers) # Remove empty strings
+outputImages = filter(None, outputImages) # Remove empty strings
 
-splitted = [s.split('-') for s in output if s.startswith('dev-')]
+def getToRemove(output):
 
-chaincodes = [ s[-2] for s in splitted]
-chaincodes = set(chaincodes)
+    splitted = [s.split('-') for s in output if s.startswith('dev-')]
 
-cache = {}
-to_remove = []
+    chaincodes = [ s[2] for s in splitted]
+    chaincodes = set(chaincodes)
 
-for idx, splitted_name in enumerate(splitted):
-    peer = splitted_name[1]
-    for chaincode in chaincodes:
-        if chaincode == splitted_name[-2]:
-            if peer in cache and chaincode in cache[peer]:
-                to_remove.append(output[idx])
-            else:
-                if peer in cache:
-                    cache[peer].append(chaincode)
+    cache = {}
+    to_remove = []
+
+    for idx, splitted_name in enumerate(splitted):
+        peer = splitted_name[1]
+        for chaincode in chaincodes:
+            if chaincode == splitted_name[2]:
+                if peer in cache and chaincode in cache[peer]:
+                    to_remove.append(output[idx])
                 else:
-                    cache[peer] = [chaincode]
+                    if peer in cache:
+                        cache[peer].append(chaincode)
+                    else:
+                        cache[peer] = [chaincode]
+    return to_remove
 
+to_remove_containers = getToRemove(outputContainers)
 
-cmds = ["docker rm -f {0}".format(name) for name in to_remove]
+to_remove_images = getToRemove(outputImages)
+
+cmds_containers = ["docker rm -f {0}".format(name) for name in to_remove_containers]
+cmds_images = ["docker rmi {0}".format(name) for name in to_remove_images]
 if DRY_RUN:
-    for cmd in cmds:
+    for cmd in cmds_containers:
+        print cmd
+    for cmd in cmds_images:
         print cmd
 else:
-    for cmd in cmds:
+    for cmd in cmds_containers:
+        call(cmd)
+    for cmd in cmds_images:
         call(cmd)
